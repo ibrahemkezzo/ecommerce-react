099479b89d3dadb4ecc9dc25845ce542d3987651
ibrahem kezzo <ibrahemkezzo.w@gmail.com>
2026-04-20 12:54:10 +0300

    finish header that contain the btmheader and top header and header.css

--- src/component/header/BtmHeader.jsx
+++ src/component/header/BtmHeader.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { IoMdMenu } from "react-icons/io";
+import { MdOutlineArrowDropDown } from "react-icons/md";
+import { Link, useLocation } from "react-router-dom";
+import { PiSignInBold } from "react-icons/pi";
+import { FaUserPlus } from "react-icons/fa6";
+
+const NavLinks = [
+    { title: "Home", link: "/" },
+    { title: "About", link: "/about" },
+    { title: "Accessories", link: "/accessories" },
+    { title: "Blog", link: "/blog" },
+    { title: "Contact", link: "/contact" },
+  ];
 
 function BtmHeader() {
+  
+  const location = useLocation();
+  const [isCategoryOpen , setIsCategoryOpen] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  // useEffect(() => {
+  //   setIsCategoryOpen(false)
+  // },[location])
+
+  useEffect(() => {
+    fetch("https://dummyjson.com/products/categories")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
   return (
-    <div>BtmHeader</div>
-  )
+    <div className="btm_header">
+      <div className="container">
+        <nav className="nav">
+          <div className="category_nav">
+            <div className="category_btn" onClick={() => setIsCategoryOpen(!isCategoryOpen)}>
+              <IoMdMenu />
+              <p>Brows Category</p>
+              <MdOutlineArrowDropDown />
+            </div>
+            <div className={`category_nav_list ${isCategoryOpen? 'active' : ''}`}>
+              <Link to={`home`}>home</Link>
+              {categories.map((category) => (
+                <Link to={category.slug}>{category.name}</Link>
+              ))}
+            </div>
+          </div>
+          <div className="nav_links">
+            {NavLinks.map((item)=>(
+              <li key={item.link} className={location.pathname === item.link ? "active" : ""}><Link to={item.link}>{item.title}</Link></li>
+            ))}
+          </div>
+        </nav>
+        <div className="sign_regs_icon">
+          <Link to="/"><PiSignInBold /></Link>
+          <Link to="/"><FaUserPlus /></Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default BtmHeader
+export default BtmHeader;
